81b76914cab5346b7995a3a959b8ae8ff7f6d655
nikitazapekin <wotblitz362@mail.ru>
2025-02-10 16:34:53 +0300

    last

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import AppRoutes from './utils/routes'
 import { BrowserRouter } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
   return (
     <>
         <BrowserRouter>
